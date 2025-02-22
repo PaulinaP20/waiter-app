f40c967d529acf37e1f14f4734c789520f38aaa1
Paulina Partyka <paulinapartyka2@gmail.com>
2025-02-22 13:21:06 +0100

    add server and all actions

--- src/components/views/Footer.js
+++ src/components/views/Footer.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import {Container, Row, Col} from 'react-bootstrap'
 
 const Footer = () => {
     return (
-        <footer>
-            <h2>Footer</h2>
+        <footer className="bg-light text-secondary py-3 mt-4">
+            <Container>
+                <Row>
+                    <Col className="text-center">
+                    <p>Copyright © PizzeriaApp 2025.</p>
+                    </Col>
+
+                </Row>
+            </Container>
+
         </footer>
     )
 };
--- src/redux/tableRedux.js
+++ src/redux/tableRedux.js
@@ -1,18 +1,46 @@
 //selectors
 
+const createActionName=actionName =>`app/tables/${actionName}`;
+
 //actions
+const FETCH_TABLES=createActionName('FETCH-TABLES');
+const FETCH_TABLE_BY_ID=createActionName('FETCH_TABLE_BY_ID');
+const UPDATE_TABLES=createActionName('UPDATE_TABLES');
 
-const createActionname=actionName =>`app/tables/${actionName}`;
 
-const UPDATE_TABLES = createActionname('UPDATE_TABLES')
+export const getTableById=(state,id)=> state.tables.find(table=>table.id===id);
 
-export const updateTables=payload => ({type: UPDATE_TABLES, payload});
+export const fetchTables=(tables)=>({type:FETCH_TABLES,payload:tables});
+export const fetchTableById=(table)=>({type:FETCH_TABLE_BY_ID, payload:table})
+export const updateTables=(payload) => ({type: UPDATE_TABLES, payload});
+
+export const loadTablesFromApi =()=> {
+  return dispatch=>{
+    fetch('http://localhost:3131/api/tables')
+      .then(res=>res.json())
+      .then(data=>dispatch(fetchTables(data)))
+  };
+};
+
+export const loadTableByIdFromApi = (id) => {
+  return dispatch=> {
+    fetch(`http://localhost:3131/tables/${id}`)
+      .then(res => res.json())
+      .then(data => dispatch(fetchTableById(data)));
+  }
+}
 
 //action creators
 const tablesReducer = (statePart = [], action) => {
     switch (action.type) {
+      case FETCH_TABLES:
+        return action.payload
+      case FETCH_TABLE_BY_ID:
+        return statePart.some(table=>table.id===action.payload.id) ? statePart.map(table=>(table.id===action.payload.id ? action.payload : table)) : [...statePart, action.payload]
       case UPDATE_TABLES:
-        return [...statePart, {...action.payload}]
+        return statePart.map(table =>
+          table.id ===action.payload.id ? {...table, ...action.payload} : table
+        )
       default:
         return statePart;
     };
